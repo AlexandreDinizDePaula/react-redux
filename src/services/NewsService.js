3f06b97bf8c8c734647a330e51c7116aa11f64dd
Alexandre Diniz <54523326+AlexandreDinizDePaula@users.noreply.github.com>
2020-03-31 10:11:31 -0300

    Update NewsService.js

--- src/services/NewsService.js
+++ src/services/NewsService.js
@@ -23,7 +23,7 @@ export class NewsService {
   static API_KEY = "d416af5d4faee64e25ab001d87aab5c3";
 
   static _baseUrl(path) {
-    return `http://newsapi.org/v2/${path}&apiKey=${NewsService.API_KEY}`;
+    return `https://newsapi.org/v2/${path}&apiKey=${NewsService.API_KEY}`;
   }
 
   static getBrasilHeadlines() {
